714c39738cf3588e35b2fb6a99898edca4659bfe
Maqsudbek <dehkanaliev@gmail.com>
2026-06-08 21:01:52 +0500

    I wrote views and forms and urls!

--- 5.2/accounts/views.py
+++ 5.2/accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import CustomUser
+from .forms import UserForm
 
 class SignUpView(View):
     def get(self, request):
-        pass
+        form = UserForm()
+        return render(request, 'sign-up.html', {'form': form})
+    
+    def post(self, request):
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')

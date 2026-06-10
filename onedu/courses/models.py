5bbeba8d3c32080ad101de20349daad01fb91b34
Maqsudbek <dehkanaliev@gmail.com>
2026-06-10 16:02:50 +0500

    Created models!

--- onedu/courses/models.py
+++ onedu/courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    desc = models.CharField(max_length=1000)
+    author = models.CharField(max_length=20)
+    duration = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.title
+    

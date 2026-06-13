992232968efea8470fc5630d611b0c2b2ad42136
Maqsudbek <dehkanaliev@gmail.com>
2026-06-13 05:02:45 +0500

    Did changes to urls.py and settings!

--- 5.5/menu/models.py
+++ 5.5/menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    CATEGORIES = (
+        ('breakfast':'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner':'Dinner')
+    )
+    
+    name = models.CharField(max_length=70)
+    category = models.CharField(max_length=20)
+    summ = models.CharField(max_length=500, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, choices=CATEGORIES)
+    
+    def __str__(self):
+        return self.name
+    
+

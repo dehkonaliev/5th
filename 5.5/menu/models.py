357b3b1a210c01e141c1557da51b2571a1758ef5
Maqsudbek <dehkanaliev@gmail.com>
2026-06-14 22:52:02 +0500

    Connecting files one by one

--- 5.5/menu/models.py
+++ 5.5/menu/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
-class Menu(models.Model):
-    CATEGORIES = (
-        ('breakfast','Breakfast'),
-        ('lunch', 'Lunch'),
-        ('dinner','Dinner')
-    )
-    
+class Menu(models.Model):    
     name = models.CharField(max_length=70)
-    category = models.CharField(max_length=20)
     summ = models.CharField(max_length=500, null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, choices=CATEGORIES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='menus/', blank=True, default='menu_default.jpg')
     
     def __str__(self):
         return self.name
--- 5.5/menu/views.py
+++ 5.5/menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Menu
 
 class ListView(View):
     def get(self, request):
-        return render(request, 'menu.html')
+        objects = Menu.objects.all()
+        return render(request, 'menu.html', {'menus':objects})

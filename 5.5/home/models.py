ae948a55d20c617b914ee1fa821ed704847c4806
Maqsudbek <dehkanaliev@gmail.com>
2026-06-15 23:22:29 +0500

    I have worked a lot on frontend

--- 5.5/home/models.py
+++ 5.5/home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Chef(models.Model):
     name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='chefs/', default='chef_default.png', blank=True)
     position = models.CharField(max_length=30)
     
     def __str__(self):
--- 5.5/home/views.py
+++ 5.5/home/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Chef
+from menu.models import Menu
+
 
 class HomeView(View):
     def get(self, request):
-        chefs = Chef.objects.all()
-        return render(request, 'index.html', {'chefs':chefs})
+        chefs = Chef.objects.all().order_by('-id')[:3]
+        menus = Menu.objects.all().order_by('-id')[:6]
+        menus_2, menus_all = menus[:2], menus[2:]
+        main_chef = Chef.objects.filter(position='main').first()
+        context = {
+            'chefs':chefs,
+            'menus_2':menus_2,
+            'menus':menus_all,
+            'main_chef':main_chef
+        }
+        return render(request, 'index.html', context)
